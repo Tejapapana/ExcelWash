9765fcc58249abea40d31077afd6d490d36d7b2e
lenovo <lenovo@DESKTOP-9GTKN1S>
2023-06-26 19:07:33 +0530

    ExcelWash project by Teja papana

---
+++ Project_ExcelWash/src/main/java/Project_ExcelWash/Project_ExcelWash/App.java
@@ -0,0 +1,13 @@
+package Project_ExcelWash.Project_ExcelWash;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
